e091187b9b17c29c64cab4677ca49781b86a4b25
João Cunha <131183584+JoaoCunha50@users.noreply.github.com>
2024-04-16 23:34:47 +0100

    Update and rename main.h to info.h

---
+++ include/info.h
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+#define ERROR 255
